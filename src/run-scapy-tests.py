c59ea0327975a48c82cc911270d2ec1172229b58
Adrian Vladu <avladu@cloudbasesolutions.com>
2020-09-07 20:14:01 +0300

    src: fixed pep8 warnings

--- src/run-scapy-tests.py
+++ src/run-scapy-tests.py
@@ -1,11 +1,26 @@
-#!/usr/bin/env python
+# Copyright 2020 Cloudbase Solutions Srl
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 import sys
-from scapy.all import *
+
+import scapy.all as scapy
 
 
 def run_scapy(target_ip):
-    packets = send(IP(dst=target_ip)/ICMP(), return_packets=True)
+    packets = scapy.send(
+        scapy.IP(dst=target_ip)/scapy.ICMP(),
+        return_packets=True)
     packets.show()
 
 run_scapy(sys.argv[1])
